7f2a5e8a15844b5b6c8768e5dd7241c8de3c8211
YounesDjelloul <younesdjelloul14@gmail.com>
2025-01-06 02:37:50 +0800

    page guards with refresh token

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -1,25 +1,37 @@
-import type {AxiosInstance} from "axios";
+import type {AxiosInstance, AxiosError} from "axios";
 import axios from "axios";
+
+let isRefreshing = false;
+let failedQueue: any[] = [];
+
+const processQueue = (error: any = null) => {
+    failedQueue.forEach(prom => {
+        if (error) {
+            prom.reject(error);
+        } else {
+            prom.resolve();
+        }
+    });
+    failedQueue = [];
+};
 
 export function createApi() {
     const config = useRuntimeConfig();
 
-    if (config.public.isDev) {
-        return constructApi('http://localhost:8000/api/');
-    }
+    const baseUrl = config.public.isDev
+        ? 'http://127.0.0.1:8000/api'
+        : 'https://younesdjelloul.pythonanywhere.com';
 
-    return constructApi('https://younesdjelloul.pythonanywhere.com');
+    return constructApi(baseUrl);
 }
 
-function constructApi(baseUrl: string) {
-    let api: AxiosInstance;
-
-    api = axios.create({
-        baseURL: baseUrl
+function constructApi(baseUrl: string): AxiosInstance {
+    const api = axios.create({
+        baseURL: baseUrl,
     });
 
     api.interceptors.request.use((config) => {
-        const accessToken = useCookie('token').value;
+        const accessToken = useCookie('access_token').value;
         if (accessToken) {
             config.headers = config.headers || {};
             config.headers['Authorization'] = `Bearer ${accessToken}`;
@@ -28,9 +40,62 @@ function constructApi(baseUrl: string) {
     }, (error) => {
         return Promise.reject(error);
     });
+
+    api.interceptors.response.use(
+        (response) => response,
+        async (error: AxiosError) => {
+            const originalRequest: any = error.config;
+
+            if (error.response?.status !== 401 || originalRequest?.url?.includes('refresh')) {
+                return Promise.reject(error);
+            }
+
+            if (isRefreshing) {
+                return new Promise((resolve, reject) => {
+                    failedQueue.push({resolve, reject});
+                })
+                    .then(() => api(originalRequest))
+                    .catch((err) => Promise.reject(err));
+            }
+
+            isRefreshing = true;
+
+            try {
+                const refreshToken = useCookie('refresh_token').value;
+                const response = await api.post('/token/refresh/', {refresh: refreshToken});
+
+                const accessTokenCookie = useCookie('token');
+                const refreshTokenCookie = useCookie('refresh_token');
+
+                accessTokenCookie.value = response.data.access;
+                if (response.data.refresh) {
+                    refreshTokenCookie.value = response.data.refresh;
+                }
+
+                originalRequest.headers['Authorization'] = `Bearer ${response.data.access_token}`;
+
+                processQueue();
+
+                return api(originalRequest);
+            } catch (refreshError) {
+                const accessTokenCookie = useCookie('access_token');
+                const refreshTokenCookie = useCookie('refresh_token');
+
+                accessTokenCookie.value = null;
+                refreshTokenCookie.value = null;
+
+                navigateTo('/login');
+
+                return Promise.reject(refreshError);
+            } finally {
+                isRefreshing = false;
+            }
+        }
+    );
+
     return api;
 }
 
 export function useApi() {
-    return createApi()
+    return createApi();
 }
