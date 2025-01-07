3baa9ae724d6369d613e5a0f70bb6fef74e82ad2
YounesDjelloul <younesdjelloul14@gmail.com>
2025-01-07 14:09:02 +0800

    new middlware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,8 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const token = useCookie('token');
-
-    // if (!token.value) {
-    //     return navigateTo('/login');
-    // }
-
+    if (to.path === '/login' && token.value) {
+        return navigateTo('/admin')
+    } else if (to.path === '/admin' && !token.value) {
+        return navigateTo('/login')
+    }
 })
