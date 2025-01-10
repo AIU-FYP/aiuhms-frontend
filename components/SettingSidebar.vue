dbefb7b3da67004c27a663cd16328955ed208415
AdnanMadi417 <164775717+AdnanMadi417@users.noreply.github.com>
2025-01-10 18:13:03 +0800

    created setting sidebar to help in authorization

---
+++ components/SettingSidebar.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+
+
+function handleLogout() {
+  useCookie('token').value = null;
+  navigateTo('/login');
+}
+
+
+
+</script>
+
+<template>
+  <div class="setting-sidebar">
+    <h1>Account Settings</h1>
+    <nav>
+      <ul class="menu">
+        <li>
+          <UIcon name="mdi-password" class="icon"/>
+          <router-link to="change-admin-password">Change Password</router-link>
+        </li>
+        <li>
+          <UIcon name="subway-admin-1" class="icon"/>
+          <router-link to="new-admin">Add New Admin</router-link>
+        </li>
+        <li>
+          <UIcon name="grommet-icons-user-admin" class="icon"/>
+          <router-link to="admin-dashboard">Admin dashboard</router-link>
+        </li>
+        <li>
+          <UIcon name="eos-icons-admin" class="icon"/>
+          <router-link to="admin">Admin</router-link>
+        </li>
+        <li class="nav-item" @click="handleLogout">
+          <UIcon name="uiw-logout" class="icon"/>
+          Logout
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+
+
+.setting-sidebar {
+  flex: 1;
+  background-color: var(--primary-color);
+  padding: 20px;
+  color: var(--text-light-color);
+  border-radius: 10px;
+  min-height: 81vh;
+}
+
+.sidebar h1 {
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
+}
+
+.menu {
+  list-style: none;
+  padding: 0;
+}
+
+.menu li {
+  display: flex;
+  align-items: center;
+  padding: .5rem;
+  margin-bottom: 15px;
+  font-size: 1rem;
+  cursor: pointer;
+  background-color: transparent;
+}
+
+.menu li:hover {
+  background-color: var(--primary-hover-color);
+  transition: .3s ease-in-out;
+}
+
+.menu li .icon {
+  margin-right: 10px;
+}
+
+.menu li a {
+  text-decoration: none;
+}
+
+
+</style>
