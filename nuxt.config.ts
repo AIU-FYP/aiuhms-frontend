03a7980eec71a905adb2ba9136377a82b559434c
YounesDjelloul <younesdjelloul14@gmail.com>
2025-01-12 20:14:02 +0800

    preset for github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,19 @@
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
-  colorMode: {
-    preference: 'light'
-  },
-  app: {
-    baseURL: '/fyp-frontend2/',
-  },
-  runtimeConfig: {
-    public: {
-      isDev: process.env.NODE_ENV === 'development',
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
+    modules: ['@nuxt/ui'],
+    colorMode: {
+        preference: 'light'
     },
-  },
+    app: {
+        baseURL: '/fyp-frontend2/',
+    },
+    nitro: {
+        preset: 'github_pages'
+    },
+    runtimeConfig: {
+        public: {
+            isDev: process.env.NODE_ENV === 'development',
+        },
+    },
 })
