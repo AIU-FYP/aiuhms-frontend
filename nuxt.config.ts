1beee731c0bf6db06d24299eae27611339d450a2
YounesDjelloul <younesdjelloul14@gmail.com>
2025-01-12 20:06:03 +0800

    set app baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
   colorMode: {
     preference: 'light'
+  },
+  app: {
+    baseURL: '/fyp-frontend2/',
   },
   runtimeConfig: {
     public: {
