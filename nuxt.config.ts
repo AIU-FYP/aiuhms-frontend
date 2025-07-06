985ac6ffa98009bec3d9bb4666571e3d192a6674
oussamachakeur <157352982+oussamachakeur@users.noreply.github.com>
2025-07-06 13:50:30 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,17 @@
 export default defineNuxtConfig({
-    ssr: false,
-    compatibilityDate: '2024-11-01',
-    devtools: {enabled: true},
-    modules: ['@nuxt/ui'],
-    colorMode: {
-        preference: 'light'
-    },
-    app: {
-        baseURL: process.env.NODE_ENV === 'development' ? '' : '/fyp-frontend2/',
-    },
-    nitro: {
-        preset: 'github_pages'
-    },
-    runtimeConfig: {
-        public: {
-            isDev: process.env.NODE_ENV === 'development',
-        },
-    },
+  ssr: false,
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui'],
+  colorMode: {
+    preference: 'light'
+  },
+  app: {
+    baseURL: '/',
+  },
+  runtimeConfig: {
+    public: {
+      isDev: process.env.NODE_ENV === 'development',
+      apiBase: 'https://fypbackend-production-ed18.up.railway.app' // or use env var
+    }
+  }
 })
