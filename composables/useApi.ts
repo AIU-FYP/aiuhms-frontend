53689b32737ae2d6dcba3e321553a864259282c8
oussamachakeur <157352982+oussamachakeur@users.noreply.github.com>
2025-07-06 15:17:55 +0800

    Update useApi.ts

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -20,7 +20,7 @@ export function createApi() {
 
     const baseUrl = config.public.isDev
         ? 'http://127.0.0.1:8000/api'
-        : 'https://fypbackend-production-d0dd.up.railway.app/api';
+        : 'https://fypbackend-production-ed18.up.railway.app/';
 
     return constructApi(baseUrl);
 }
