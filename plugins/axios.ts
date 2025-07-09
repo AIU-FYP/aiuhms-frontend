9f2d0c673447d66287a44cc660b399a7255262de
adnanmadi417 <uso53dr6i8s4yt0idkq7f6ivj04en8@bots.bitbucket.org>
2025-07-09 11:41:16 +0800

    removed console log from axios file

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -1,12 +1,3 @@
-// import axios from 'axios'
-//
-// export default defineNuxtPlugin(() => {
-//   const config = useRuntimeConfig()
-//   axios.defaults.baseURL = config.public.apiBase
-// })
-
-
-// plugins/axios.ts
 import axios from 'axios';
 
 export default defineNuxtPlugin((nuxtApp) => {
@@ -14,8 +5,6 @@ export default defineNuxtPlugin((nuxtApp) => {
 
   axios.defaults.baseURL = config.public.apiBase;
   axios.defaults.withCredentials = true;
-
-  console.log('✅ Axios plugin loaded with baseURL:', axios.defaults.baseURL); // Debugging
 
   nuxtApp.provide('axios', axios);
 });
